a1155fe886aab41534a9bc26d614e56b7f77a2e4
pathikrit <pathikritbhowmick@msn.com>
2017-04-12 18:32:03 -0400

    rearrange code

--- build.sbt
+++ build.sbt
@@ -3,10 +3,7 @@ version := "2.0.0"
 description := "Scala macros to generate RESTy models"
 organization := "com.github.pathikrit"
 scalaVersion := "2.11.8"
-crossScalaVersions := Seq("2.11.8")
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:experimental.macros")
-resolvers ++= Seq(Resolver.typesafeRepo("releases"))
-libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
 libraryDependencies ++= Seq(
   "org.scalameta" %% "scalameta" % "1.6.0",
   "org.scalatest" %% "scalatest" % "3.0.1" % Test
--- src/main/scala/com/github/pathikrit/metarest/Resource.scala
+++ src/main/scala/com/github/pathikrit/metarest/Resource.scala
@@ -12,38 +12,37 @@ class patch extends StaticAnnotation
 @compileTimeOnly("@metarest.Resource not expanded")
 class Resource extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    def generate(clazz: Defn.Class, companion: Defn.Object) = clazz.ctor match {
-      case Ctor.Primary(_, _, paramss) =>
-        val fieldsWithModifier = for {
-          Term.Param(mods, name, decltype, default) <- paramss.flatten
-          modifier <- mods
-        } yield modifier match {
-          case mod"@get" | mod"@put" | mod"@post" => Some(modifier -> Term.Param(Nil, name, decltype, default))
-          case mod"@patch" => Some(modifier -> Term.Param(Nil, name, decltype, None))
-          case _ => None
-        }
-
-        val fields = fieldsWithModifier.flatten
-          .groupBy({case (mod, _) => mod.toString.stripPrefix("@").capitalize})
-          .mapValues(values => values.map(_._2))
-
-        val newFields = fields map { case (className, classParams) =>
-          q"case class ${Type.Name(className)}(..$classParams)"
-        }
-
-        val newCompanion = companion.copy(
-          templ = companion.templ.copy(stats = Some(
-            companion.templ.stats.getOrElse(Nil) ++ newFields
-          ))
-        )
-
-        Term.Block(Seq(clazz, newCompanion))
-    }
-
-    defn match {
-      case Term.Block(Seq(cls: Defn.Class, companion: Defn.Object)) => generate(cls, companion)
-      case cls @ Defn.Class(_, name, _, ctor, _) => generate(cls, q"object ${Term.Name(name.value)} { }")
+    val (cls: Defn.Class, companion: Defn.Object) = defn match {
+      case Term.Block(Seq(cls: Defn.Class, companion: Defn.Object)) => (cls, companion)
+      case cls @ Defn.Class(_, name, _, _, _) => (cls, q"object ${Term.Name(name.value)} {}")
       case _ => abort("@metarest.Resource must annotate a class")
     }
+
+    val Ctor.Primary(_, _, paramss) = cls.ctor
+
+    val fieldsWithModifier = for {
+      Term.Param(mods, name, decltype, default) <- paramss.flatten
+      modifier <- mods
+    } yield modifier match {
+      case mod"@get" | mod"@put" | mod"@post" => Some(modifier -> Term.Param(Nil, name, decltype, default))
+      case mod"@patch" => Some(modifier -> Term.Param(Nil, name, decltype, None))
+      case _ => None
+    }
+
+    val fields = fieldsWithModifier.flatten
+      .groupBy({case (mod, _) => mod.toString.stripPrefix("@").capitalize})
+      .mapValues(values => values.map(_._2))
+
+    val newFields = fields map { case (className, classParams) =>
+      q"case class ${Type.Name(className)}(..$classParams)"
+    }
+
+    val newCompanion = companion.copy(
+      templ = companion.templ.copy(stats = Some(
+        companion.templ.stats.getOrElse(Nil) ++ newFields
+      ))
+    )
+
+    Term.Block(Seq(cls, newCompanion))
   }
 }
