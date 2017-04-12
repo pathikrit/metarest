7e457ae85d415477b46e82fd1b428bfd76da2f86
pathikrit <pathikritbhowmick@msn.com>
2017-04-12 17:40:05 -0400

    It works ... except PATCH

--- build.sbt
+++ build.sbt
@@ -1,27 +1,14 @@
 name := "metarest"
-
 version := "2.0.0"
-
 description := "Scala macros to generate RESTy models"
-
-licenses += ("MIT", url("http://opensource.org/licenses/MIT"))
-
 organization := "com.github.pathikrit"
-
 scalaVersion := "2.11.8"
-
 crossScalaVersions := Seq("2.11.8")
-
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-language:experimental.macros")
-
-resolvers ++= Seq(
-  Resolver.typesafeRepo("releases")
-)
-
+resolvers ++= Seq(Resolver.typesafeRepo("releases"))
 libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _)
-
 libraryDependencies ++= Seq(
+  "org.scalameta" %% "scalameta" % "1.6.0",
   "org.scalatest" %% "scalatest" % "3.0.1" % Test
 )
-
 addCompilerPlugin("org.scalameta" % "paradise" % "3.0.0-M7" cross CrossVersion.full)
--- src/main/scala/com/github/pathikrit/metarest/Resource.scala
+++ src/main/scala/com/github/pathikrit/metarest/Resource.scala
@@ -1,74 +1,49 @@
 package com.github.pathikrit.metarest
 
-import scala.annotation.StaticAnnotation
-import scala.reflect.macros.blackbox
-
-class Resource extends StaticAnnotation {
-  def macroTransform(annottees: Any*): Any = macro Resource.impl
-}
+import scala.collection.immutable.Seq
+import scala.annotation.{StaticAnnotation, compileTimeOnly}
+import scala.meta._
 
 class get extends StaticAnnotation
 class put extends StaticAnnotation
 class post extends StaticAnnotation
 class patch extends StaticAnnotation
 
-object Resource {
-  def impl(c: blackbox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
-    import c.universe._
-
-    def withCompileError[A](msg: String)(block: => A): A = try block catch {
-      case e: MatchError => c.abort(c.enclosingPosition, s"MetaRest: $msg; Got: $e")
-    }
-
-    def getClassAndFields(classDecl: ClassDef) = withCompileError("must annotate a case class") {
-      val q"case class $className(..$fields) extends ..$bases { ..$body }" = classDecl
-      (className, fields)
-    }
-
-    def generateModels(fields: List[ValDef]) = {
-      val fieldsWithAnnotations = fields.map(_.mods.annotations).zip(fields)
-      val newFields = fieldsWithAnnotations flatMap { case (annotations, field) =>
-        annotations.map(_ -> field) collect {
-          case (q"new patch", q"$accessor val $vname: $tpe") => "patch" -> q"$accessor val $vname: Option[$tpe] = None"
-          case (q"new $annotation", f) if Set("get", "post", "put") contains annotation.toString => annotation.toString -> f.duplicate
+@compileTimeOnly("@metarest.Resource not expanded")
+class Resource extends StaticAnnotation {
+  inline def apply(defn: Any): Any = meta {
+    def generate(clazz: Defn.Class, companion: Defn.Object) = clazz.ctor match {
+      case Ctor.Primary(_, _, paramss) =>
+        val fieldsWithModifier = for {
+          Term.Param(mods, name, decltype, default) <- paramss.flatten
+          modifier <- mods
+        } yield modifier match {
+          case mod"@get" | mod"@put" | mod"@post" => Some(modifier -> Term.Param(Nil, name, decltype, default))
+          case mod"@patch" => Some(modifier -> Term.Param(Nil, name, decltype, None))
+          case _ => None
         }
-      }
-      newFields.groupBy(_._1) map { case (annotation, values) =>
-        val (className, classFields) = (TypeName(annotation.capitalize), values.map(_._2))
-        q"case class $className(..$classFields)"
-      }
+
+        val fields = fieldsWithModifier.flatten
+          .groupBy({case (mod, _) => mod.toString.stripPrefix("@").capitalize})
+          .mapValues(values => values.map(_._2))
+
+        val newFields = fields map { case (className, classParams) =>
+          q"case class ${Type.Name(className)}(..$classParams)"
+        }
+
+        val newCompanion = companion.copy(
+          templ = companion.templ.copy(stats = Some(
+            companion.templ.stats.getOrElse(Nil) ++ newFields
+          ))
+        )
+
+        Term.Block(Seq(clazz, newCompanion))
     }
 
-    def modifiedDeclaration(classDecl: ClassDef, compDeclOpt: Option[ModuleDef] = None) = {
-      val (className, fields) = getClassAndFields(classDecl)
-
-      val compDecl = compDeclOpt map { compDecl =>
-        val q"object $obj extends ..$bases { ..$body }" = compDecl
-        q"""
-          object $obj extends ..$bases {
-            ..$body
-            ..${generateModels(fields)}
-          }
-        """
-      } getOrElse {
-        q"""
-          object ${className.toTermName} {
-            ..${generateModels(fields)}
-          }
-         """
-      }
-
-      c.Expr( q"""
-        $classDecl
-        $compDecl
-      """)
-    }
-
-    withCompileError("must annotate a class") {
-      annottees.map(_.tree) match {
-        case (classDecl: ClassDef) :: Nil => modifiedDeclaration(classDecl)
-        case (classDecl: ClassDef) :: (compDecl: ModuleDef) :: Nil => modifiedDeclaration(classDecl, Some(compDecl))
-      }
+    defn match {
+      case Term.Block(Seq(cls: Defn.Class, companion: Defn.Object)) => generate(cls, companion)
+      case cls @ Defn.Class(_, name, _, ctor, _) => generate(cls, q"object ${Term.Name(name.value)} { }")
+      case _ => abort("@metarest.Resource must annotate a class")
     }
   }
 }
