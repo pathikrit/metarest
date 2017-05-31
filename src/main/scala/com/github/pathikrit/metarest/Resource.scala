760cccebc4122fb0f2c7a89b14ee47a8dab53895
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2017-05-31 21:35:28 +0200

    Simplify code

--- src/main/scala/com/github/pathikrit/metarest/Resource.scala
+++ src/main/scala/com/github/pathikrit/metarest/Resource.scala
@@ -11,28 +11,28 @@ class patch extends StaticAnnotation
 
 class Resource extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    val (cls: Defn.Class, companion: Defn.Object) = defn match {
+    val (cls, companion) = defn match {
       case q"${cls: Defn.Class}; ${companion: Defn.Object}" => (cls, companion)
-      case cls: Defn.Class => (cls, q"object ${Term.Name(cls.name.value)} {}")
+      case cls: Defn.Class => (cls, q"object ${Term.Name(cls.name.value)}")
       case _ => abort("@metarest.Resource must annotate a class")
     }
 
     val paramsWithAnnotation = for {
-      Term.Param(mods, name, decltype, default) <- cls.ctor.paramss.flatten
+      param <- cls.ctor.paramss.flatten
       seenMods = mutable.Set.empty[String]
-      modifier <- mods if seenMods.add(modifier.toString)
-      (tpe, defArg) <- modifier match {
-        case mod"@get" | mod"@put" | mod"@post" => Some(decltype -> default)
-        case mod"@patch" =>
-          val optDeclType = decltype.collect({case tpe: Type => targ"Option[$tpe]"})
-          val defaultArg = default match {
+      modifier <- param.mods if seenMods.add(modifier.toString)
+      newParam <- modifier match {
+        case mod"@get" | mod"@put" | mod"@post" => Some(param.copy(mods = Nil))
+        case mod"@patch" => param.decltpe collect { case tpe: Type =>
+          val defaultArg = param.default match {
             case Some(term) => q"Some($term)"
             case None => q"None"
           }
-          Some(optDeclType -> Some(defaultArg))
+          param"${param.name}: Option[$tpe] = $defaultArg"
+        }
         case _ => None
       }
-    } yield modifier -> param"$name: $tpe = $defArg"
+    } yield modifier -> newParam
 
     val models = paramsWithAnnotation
       .groupBy(_._1.toString)
@@ -42,10 +42,10 @@ class Resource extends StaticAnnotation {
         q"case class $className[..${cls.tparams}] (..$classParams)"
       })
 
-    val newCompanion = companion.copy(
-      templ = companion.templ.copy(stats = Some(
-        companion.templ.stats.getOrElse(Nil) ++ models
-      ))
+   val newCompanion = companion.copy(
+      templ = companion.templ.copy(
+        stats = Some(companion.templ.stats.getOrElse(Nil) ++ models)
+      )
     )
 
     q"$cls; $newCompanion"
