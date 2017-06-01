4b98d3e0478cee0531f49484f0a88447c5c19757
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2017-06-01 12:03:04 +0200

    Simplify code

--- src/main/scala/com/github/pathikrit/metarest/Resource.scala
+++ src/main/scala/com/github/pathikrit/metarest/Resource.scala
@@ -23,24 +23,24 @@ class Resource extends StaticAnnotation {
       modifier <- param.mods if seenMods.add(modifier.toString)
       newParam <- modifier match {
         case mod"@get" | mod"@put" | mod"@post" => Some(param.copy(mods = Nil))
-        case mod"@patch" =>
-          val tpe = param.decltpe.get.asInstanceOf[Type]
+        case mod"@patch" => param.decltpe collect { case tpe: Type =>
           val defaultArg = param.default match {
             case Some(term) => q"Some($term)"
             case None => q"None"
           }
-          Some(param"${param.name}: Option[$tpe] = ${defaultArg}")
+          param"${param.name}: Option[$tpe] = $defaultArg"
+        }
         case _ => None
       }
     } yield modifier -> newParam
 
     val grouped = paramsWithAnnotation
-      .groupBy(_._1.toString())
+      .groupBy(_._1.toString)
       .mapValues(_.map(_._2))
 
     val models = grouped.map({case (annotation, classParams) =>
       val className = Type.Name(annotation.stripPrefix("@").capitalize)
-      q"case class ${className}[..${cls.tparams}] (..$classParams)"
+      q"case class ${className}[..${cls.tparams}](..$classParams)"
     })
 
     val newCompanion = companion.copy(
